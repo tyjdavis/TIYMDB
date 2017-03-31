862d517636e00f3998409e5ebeb7065f5db7d0c2
Tyler Davis <tyjdavis@outlook.com>
2017-03-31 16:05:56 -0400

    pulling runtime

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
 
-let contextFromAPI = fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`)
+let nowPlayingFromAPI = fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`)
 .then(response => response.json())
-.then(object => object.results)
-.then(placeInTemplate)
-//.then(arr => console.log(arr)); //array of objects now stored in contextFromAPI
+.then(object => object.results) //results not pickles
+.then(getDetailsFromAPI)
 
-let source   = document.querySelector("#movies-now-playing-list").innerHTML;
+
+
+function getDetailsFromAPI (arr) {
+let promises = arr.map( (movie, index) => { //map (element that we're iterating over (objects within result), 0-19)
+  return fetch (`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${key}&language=en-US`)
+  .then(response => response.json())
+  .then(movie => {
+    arr[index].runtime = movie.runtime;
+    //arr[index].myNewProp = 'Hello';
+    return arr[index];})
+});
+  Promise.all(promises).then(placeInTemplate);
+}
+
+
+//fetch (`https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=${key}&language=en-US`)
+
+
+
+
+let source = document.querySelector("#movies-now-playing-list").innerHTML;
 let template = Handlebars.compile(source);
-
-
 
 function placeInTemplate (movies) {
       let html = movies.map(obj => template(obj)).join('');
       let destination = document.querySelector('.handlebars-demo');
       destination.innerHTML = html;
 }
-
-
-
-
-
-/*
-function useJsonOnPage (json) {
-  let name = document.querySelector('.movieName');
-  name.textContent = json.original_title;
-  let poster = document.querySelector('.movie_poster');
-  poster.src = "https://image.tmdb.org/t/p/w500" + json.poster_path;
-  let tagline = document.querySelector('.tagline');
-  tagline.textContent = json.tagline;
-  let home = document.querySelector('.synopsis');
-  home.textContent = json.overview;
-  let releaseDate = document.querySelector('.date');
-  releaseDate.textContent = json.release_date;
-}
-
-fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=45569efe8c8f75d4edf172762038dbc1&language=en-US&page=1`)
-.then(response => response.json())  //converting to json
-.then(json => useJsonOnPage(json)); */
